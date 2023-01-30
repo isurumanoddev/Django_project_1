9b7fa34b939647b51878869610d9ee664173b6a0
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-01-30 11:29:06 +0530

    databse models

--- base/views.py
+++ base/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render
-
-rooms = [
-    {"id": 1, "name": "Lets learn something"},
-    {"id": 2, "name": "Deeper learn something"},
-    {"id": 3, "name": "Front matter "},
-    {"id": 4, "name": "Front matter "},
-    {"id": 5, "name": "Front matter "},
-    {"id": 6, "name": "Front matter "},
-    {"id": 7, "name": "Front matter "}
-]
+from .models import Room
+#
+# rooms = [
+#     {"id": 1, "name": "Lets learn something"},
+#     {"id": 2, "name": "Deeper learn something"},
+#     {"id": 3, "name": "Front matter "},
+#     {"id": 4, "name": "Front matter "},
+#     {"id": 5, "name": "Front matter "},
+#     {"id": 6, "name": "Front matter "},
+#     {"id": 7, "name": "Front matter "}
+# ]
 
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     return render(request, "home.html", {'rooms': rooms})
 
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i["id"] == int(pk):
-            room = i
-            print("fount")
-
+    room = Room.objects.get(id=pk)
     context = {"room": room}
 
-    return render(request, "about.html",context)
+    return render(request, "room.html", context)
