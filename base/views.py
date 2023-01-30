a5f7accd330ad7fb906bd8a719c5977e515cbf4a
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-01-30 14:25:01 +0530

    creating Crud

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
 
@@ -25,4 +25,25 @@ def create_room(request):
             return redirect("home")
     context = {"form": form}
     return render(request, "room_form.html", context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "room_form.html", context)
+
+def delete_room(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+
+    return render(request,"delete.html",{"obj":room})
 
