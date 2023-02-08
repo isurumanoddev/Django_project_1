84d43f9a3a194f0a25898157416351a15b1ba139
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-02-08 19:24:46 +0530

    complete login UI improve

--- base/views.py
+++ base/views.py
@@ -33,7 +33,7 @@ def login_page(request):
             print("User not registered")
 
     context = {"page": page}
-    return render(request, "a.html", context)
+    return render(request, "login_register.html", context)
 
 
 def logout_user(request):
@@ -51,7 +51,7 @@ def user_register(request):
             return redirect("login")
 
     context = {"form": form}
-    return render(request, "a.html", context)
+    return render(request, "login_register.html", context)
 
 
 def home(request):
