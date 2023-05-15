c88af9877b81d58896df0054fcb4504a30992290
isurumanod <isurumanod99@gmail.com>
2023-05-15 14:23:01 +0530

    studybud ui update

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,18 @@
 from django.forms import ModelForm
-from .models import Room
+from .models import Room,Message
+
+
 
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields = "__all__"
-        exclude =["host","participants"]
+        fields = ["name","topic","description"]
+
+
+    def __init__(self, *args, **kwargs):
+        super(RoomForm,self).__init__(*args,**kwargs)
+
+        self.fields["name"].widget.attrs.update({'class':'form__group-input'})
+        self.fields["topic"].widget.attrs.update({'class':'form__group-input'})
+        self.fields["description"].widget.attrs.update({'class':'form__group-textarea'})
